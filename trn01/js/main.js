aba9e69bd22c5fd919853be6b6fb7c5610fdc79a
peonyblue <105770841+peonyblue@users.noreply.github.com>
2022-05-27 18:12:53 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,9 +1,17 @@
 $(function(){
 
+    $('.main_slider').on('inIt reInit afterChange', function () {
+        var here = $('.slick-current');
+        here.addClass('on').siblings().removeClass('on')
+    })
+
+
     $('.main_slider').slick({
         arrows: false,
         autoplay: true,
         pauseOnHover: false,
+        pauseOnFocus: false,
+        fade: true,
         // 대소문자 구분
     });
 });

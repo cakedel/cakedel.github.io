7cbd6fa5df32b08368f3c19ab610a55018a1133b
peonyblue <coalgreyy9@gmail.com>
2022-06-22 15:44:24 +0900

    22.06.22 index 수정

---
+++ hyundai_ec/js/hyundai.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.slogan .slide').slick({
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: '350px'
+    });
+})

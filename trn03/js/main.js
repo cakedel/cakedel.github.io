cb1c1cf31f95a492b3cf84738e445d2ab2a2deb5
peonyblue <coalgreyy9@gmail.com>
2022-07-29 17:44:53 +0900

    22.07.29 responsiveWeb

--- trn03/js/main.js
+++ trn03/js/main.js
@@ -5,4 +5,10 @@ $(function () {
     $('.pop button').on('click', function () {
         $(this).parent().hide();
     })
+    $('.mainSlider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
 })

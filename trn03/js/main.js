f9c8663d3cce10401b67efbef88260bb979ec49e
peonyblue <coalgreyy9@gmail.com>
2022-07-29 09:27:33 +0900

    22.07.28 responsiveWeb

---
+++ trn03/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.TopBanner i').on('click', function () {
+        $('.TopBanner').slideUp();
+    })
+    $('.pop button').on('click', function () {
+        $(this).parent().hide();
+    })
+})

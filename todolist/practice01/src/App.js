41cf974a28bf271fe9e6ec314ef3ff7f35dac9b2
cakedel <coalgreyy9@gmail.com>
2022-10-06 06:56:22 +0900

    todolist practice

--- todolist/practice01/src/App.js
+++ todolist/practice01/src/App.js
@@ -1,39 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
-import react, { useState, useRef } from "react";
+import react, { useRef, useState } from "react";
+
 function App() {
   const [input, setInput] = useState({});
   const [list, setList] = useState([]);
   const num = useRef(1);
-  const registerList = () => {
-    setList([...list, input]);
-    console.log(list);
-    setInput({ ...input, value: "" });
-    num.current++;
-  };
   const handleInput = (e) => {
     const { name, value } = e.target;
     setInput({ ...input, [name]: value, id: num.current });
-    console.log(e.target.value);
   };
-  const removeTodo = (id) => {
-    setList(list.filter((el) => id !== el.id));
+  const registerTodo = () => {
+    setList([...list, input]);
+    num.current++;
+    setInput({ ...input, title: "" });
+  };
+  const deleteTodo = (idx) => {
+    setList(list.filter((el) => idx !== el.id));
     console.log(list);
   };
   return (
     <div className="App">
-      <h2>ToDoList</h2>
+      <h2>할일 만들기</h2>
       <ul>
         {list.map((el, idx) => {
           return (
             <li key={idx}>
-              {el.title} <button onClick={() => removeTodo(el.id)}>Del</button>
+              {el.title}
+              <button onClick={() => deleteTodo(el.id)}>del</button>
             </li>
           );
         })}
       </ul>
-      <input type="text" name="title" onChange={handleInput} />
-      <button onClick={registerList}>Register</button>
+
+      <input
+        type="text"
+        value={input.title}
+        name="title"
+        onChange={handleInput}
+      />
+      <button onClick={registerTodo}>submit</button>
     </div>
   );
 }
